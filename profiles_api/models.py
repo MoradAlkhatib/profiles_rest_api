16e6f61891bbd335c2d0951f5d563eff57dcdf7d
moradalkhatib <muradalkhateb@icloud.com>
2022-01-18 19:29:58 +0200

    define ManagerUser custom model

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -1,9 +1,35 @@
-import email
-from statistics import mode
-from unicodedata import name
 from django.db import models
-
 from django.contrib.auth.models import AbstractBaseUser , PermissionsMixin
+from django.contrib.auth.models import BaseUserManager 
+
+
+class UserProfileManager(BaseUserManager):
+    """Manager for User Profiles"""
+
+    def create_user(self,email,name,password=None):
+        """create a new user profile"""
+        if not email:
+            raise ValueError('User Must Have Email Address')
+
+        email = self.normalize_email(email)
+        user = self.model(email=email,name=name)
+
+        user.set_password(password)
+        user.save(using=self._db)     
+
+        return user
+        
+    def create_superuser(self,email,name,password):
+        """create and save a new superuser with given details"""
+        user = self.create_user(email,name,password)
+
+        user.is_superuser = True
+        user.is_staff = True
+        user.save(using=self._db)
+
+        return user
+
+
 
 
 class UserProfile(AbstractBaseUser,PermissionsMixin):
@@ -13,7 +39,7 @@ class UserProfile(AbstractBaseUser,PermissionsMixin):
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
 
-    # objects =UserProfileManager()
+    objects =UserProfileManager()
     USERNAME_FIELD ='email'
     REQUIRED_FIELDS = ['name']
 
