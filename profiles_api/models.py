62547a5e67566f85c7e2e5103662b58b4ae01e68
moradalkhatib <muradalkhateb@icloud.com>
2022-01-18 18:54:01 +0200

    define user custom user model

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -1,3 +1,30 @@
+import email
+from statistics import mode
+from unicodedata import name
 from django.db import models
 
-# Create your models here.
+from django.contrib.auth.models import AbstractBaseUser , PermissionsMixin
+
+
+class UserProfile(AbstractBaseUser,PermissionsMixin):
+    """Database model for users in the system"""
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    # objects =UserProfileManager()
+    USERNAME_FIELD ='email'
+    REQUIRED_FIELDS = ['name']
+
+    def get_full_name(self):
+        """Retrieve Full Name of User"""
+        return self.name
+
+    def __str__(self) :
+        """Return String Representation of Our User"""
+        return self.email
+
+
+
+
