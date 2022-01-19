c6e8a89a6cdc03b7b320f8db7825ad6294c9ab40
moradalkhatib <muradalkhateb@icloud.com>
2022-01-19 19:08:36 +0200

    added post function to the HelloApiView

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,8 +1,9 @@
+from os import name
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
 from .serializers import HelloSerializer
-
+from rest_framework import status
 
 class HelloApiView(APIView):
     """Test API View"""
@@ -18,3 +19,16 @@ class HelloApiView(APIView):
             'Is mapped manually to URLs',
         ] 
         return Response({'message':'Hello','an_apiview':an_apiview})
+    
+    def post(self,request):
+        """Create a hello message with our name"""
+
+        serializer = self.serializer_class(data = request.data)
+
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message = f'Hello {name}'
+            return Response({'message':message})
+        else :
+            return Response(serializer.errors,
+            status = status.HTTP_400_BAD_REQUEST)
