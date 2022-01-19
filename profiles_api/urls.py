ea58fc5e3cdd46b99c14edb5421f274f854943ad
moradalkhatib <muradalkhateb@icloud.com>
2022-01-19 21:29:49 +0200

    create default router and register the HelloViewSet into it

--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -1,9 +1,16 @@
 import fractions
 
 
-from django.urls import path
-from .views import HelloApiView
+from django.urls import path ,include
+from .views import HelloApiView , HelloViewSet
+from rest_framework.routers import DefaultRouter
+
+# create the Router 
+router = DefaultRouter()
+# register HelloViewSet to the Router
+router.register('hello-viewset' , HelloViewSet , basename='hello-viewset')
 
 urlpatterns = [
     path('hello-view/',HelloApiView.as_view()),
+    path('',include(router.urls))
 ]
